7893e0e41680630ffc0fe7f61f579bb0286a77f3
Ilia Prosianoi <iprosyanoy@ya.ru>
2026-07-16 18:47:55 +0500

    render localized content directly in React

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { applyContentEnhancements } from './contentEnhancements.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-queueMicrotask(applyContentEnhancements)
