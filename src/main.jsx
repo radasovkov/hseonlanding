0a9dcd368f236a74409f3c2d727e6ef6d024256c
Ilia Prosianoi <iprosyanoy@ya.ru>
2026-07-16 21:11:19 +0500

    mount language dropdown and UI fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './uiFixes.css'
+import { applyLanguageSelect } from './languageSelect.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+queueMicrotask(applyLanguageSelect)
